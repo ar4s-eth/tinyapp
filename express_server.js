6a81a8fe92aaab096aa1b74678850e83ea3510d4
Ashley Barr <ashley.barr@ar4s.com>
2020-12-07 21:44:46 +0000

    added a json handler for urlDatabase

--- express_server.js
+++ express_server.js
@@ -4,13 +4,17 @@ const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xk": "http:www.google.com"
+  "9sm5xk": "http://www.google.com"
 };
 
 //register a handler on the root path
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
